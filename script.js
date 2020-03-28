500f2e2f76ed25e1e1e760a1f93901c690bed941
Rachelik <rachel.ik.87@gmail.com>
2020-03-28 19:47:37 +0800

    Add ask player's name function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("It is running!")
+//Ask for Player's Name
+var playerInput = document.querySelector('#player-input');
+var playerName = document.querySelector('#player-name');
+var submitBtn = document.querySelector('#submit-btn');
+
+var enterPlayerName = function() {
+    playerName.innerHTML = "Hello "+playerInput.value;
+    playerInput.style.display = "none";
+    submitBtn.style.display = "none";
+}
+
+submitBtn.addEventListener('click', enterPlayerName);
