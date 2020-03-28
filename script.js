fb964edb43b3759f869a7a30cab118a24181c2bc
Rachelik <rachel.ik.87@gmail.com>
2020-03-28 20:13:02 +0800

    add reset button function

--- script.js
+++ script.js
@@ -3,11 +3,30 @@ console.log("It is running!")
 var playerInput = document.querySelector('#player-input');
 var playerName = document.querySelector('#player-name');
 var submitBtn = document.querySelector('#submit-btn');
+var resetBtn = document.querySelector('#reset-btn')
+var score = document.querySelector('#score');
 
 var enterPlayerName = function() {
-    playerName.innerHTML = "Hello "+playerInput.value;
-    playerInput.style.display = "none";
-    submitBtn.style.display = "none";
+    //get name and change to upper case to greet
+    playerName.innerHTML = "Hello "+playerInput.value.toUpperCase();
+    //hide input text-box and style away.
+    playerInput.classList.add('hide');
+    submitBtn.classList.add('hide');
+    playerName.classList.add('style-change');
 }
 
+//When submit button is clicked, name is fixed.
 submitBtn.addEventListener('click', enterPlayerName);
+
+
+//reset everything
+var resetGame = function() {
+    playerInput.value = "";
+    playerName.innerText = "Player's Name";
+    playerInput.classList.remove('hide');
+    submitBtn.classList.remove('hide');
+    playerName.classList.remove('style-change');
+    score.innerText = 0;
+}
+
+resetBtn.addEventListener('click', resetGame);
