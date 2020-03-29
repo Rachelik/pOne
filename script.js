4957432bfbb2192a754ecd9cbe095be918280ea2
Rachelik <rachel.ik.87@gmail.com>
2020-03-29 22:27:16 +0800

    Hard code to get the required results. To improve.

--- script.js
+++ script.js
@@ -13,47 +13,48 @@ var totalscores = 0;
 
 
 //array for cards
-var symbols = ["`", "-", "=", "[", "]", ";", "'", ",", ".", "/", "|", "\u005C"];
+var symbols = ["`", "-", "=", "[", "]", ";", "'", ",", ".", "/", "\u005C"];
 
 
 //generate two random cards.
-var genCardIArr = [];
-var genCards = function() {
-    while (genCardIArr.length < 2) {
+var cardPickTwo = [];
+var cardPick = function() {
+    while (cardPickTwo.length < 2) {
         var genCardI = Math.floor(Math.random() * 5);
-        if (genCardIArr.indexOf(genCardI) === -1) {
-            genCardIArr.push(genCardI);
+        if (cardPickTwo.indexOf(genCardI) === -1) {
+            cardPickTwo.push(genCardI);
         };
     };
-    console.log("Cards Index: "+genCardIArr);
+    console.log("Cards Index: "+cardPickTwo);
 };
 
 
 //generate two random symbols.
-var genSymIArr = [];
-var genSyms = function() {
-    while (genSymIArr.length < 2) {
-        var genSymI = Math.floor(Math.random() * 12);
-        if (genSymIArr.indexOf(genSymI) === -1) {
-            genSymIArr.push(genSymI);
+var symPickTwo = [];
+var symPick = function() {
+    while (symPickTwo.length < 2) {
+        var genSymI = Math.floor(Math.random() * 11);
+        if (symPickTwo.indexOf(genSymI) === -1) {
+            symPickTwo.push(genSymI);
         };
     };
-    console.log("Symbols Index: "+genSymIArr);
+    console.log("Symbols Index: "+symPickTwo);
 };
 
 
-var genCardsSyms = function(cardsIArr, symsIArr) {
-    for (var i = 0; i<cardsIArr.length; i++) {
-        cards[cardsIArr[i]].innerText = symbols[symsIArr[i]];
+
+var linkCardSym = function(cardPickArr, symPickArr) {
+    for (var i = 0; i<cardPickArr.length; i++) {
+        cards[cardPickArr[i]].innerText = symbols[symPickArr[i]];
     };
 };
+
 
 
 var enterPlayerName = function(event) {
     if (event.key === 'Enter') {
     //get name and change to upper case to greet
     playerName.innerHTML = "Hello "+playerInput.value.toUpperCase();
-
     //hide input text-box and style away.
     playerInput.classList.add('hide');
     playerName.classList.add('style-change');
@@ -62,6 +63,7 @@ var enterPlayerName = function(event) {
 
 //When Enter Key is clicked, name is fixed.
 playerInput.addEventListener('keypress', enterPlayerName);
+
 
 //reset everything
 var resetGame = function() {
@@ -72,46 +74,78 @@ var resetGame = function() {
     playerName.classList.remove('style-change');
     score.innerText = 0;
 };
+
 //When reset button is clicked, everything reset.
 resetBtn.addEventListener('click', resetGame);
+
 
 var playInputReset = function() {
     playInput.value = "";
 };
 
-var genCardSet = function() {
+
+
+var makeCardSet = function() {
     playInput.placeholder = "";
-    genCards();
-    genSyms();
-    genCardsSyms(genCardIArr, genSymIArr);
+    cardPick();
+    symPick();
+    linkCardSym(cardPickTwo, symPickTwo);
     playInputReset();
-    gameStatus = "please type in"
 };
 
 
-var typedCorrectKeyArr = [];
-var typedWrongKeyArr = [];
+var correctCardArr = [];
+var wrongCardArr = [];
+var game = "start";
+var symTyped;
 var checkInput = function(event) {
     if (event.key === 'Enter') {
-        typedKey = playInput.value;
-        if (typedKey === "start") {
-            genCardSet();
-        } else if (symbols.indexOf(typedKey) === -1) {
-            console.log("wrong. please try again");
-
+        symTyped = playInput.value
+        //whatever typed will be the playInput.value in the beginning.
+        if (symTyped === "start") {
+            makeCardSet();
             playInputReset();
-        } else {
-            while(typedCorrectKeyArr.length < 2) {
-                var typedKeySymI = symbols.indexOf(typedKey);
-                var typedKeyCardI = genSymIArr.indexOf(typedKeySymI);
-                cards[typedKeyCardI].innerText = " ";
+            // symTyped = playInput.value
+        } else if (symTyped !== symbols[symPickTwo[0]] && symTyped !== symbols[symPickTwo[1]]) {
+            if (wrongCardArr.indexOf(symTyped) === -1){
+                wrongCardArr.push(symTyped);
+                totalscores--;
+                score.innerText = totalscores;
                 playInputReset();
-                if (typedCorrectKeyArr.indexOf(typedKeyCardI) === -1) {
-                    console.log("correct");
-                    typedCorrectKeyArr.push(typedKeySymI);
-                } else {
-                    console.log("You have already typed that.");
+            } else if (wrongCardArr.indexOf(symTyped) !== -1) {
+                console.log("you have entered this false value before.")
+                playInputReset();
+            };
+        } else if (symTyped === symbols[symPickTwo[0]] || symTyped === symbols[symPickTwo[1]]) {
+            if (correctCardArr.indexOf(symTyped) === -1) {
+                correctCardArr.push(symTyped);
+                if (symTyped === cards[cardPickTwo[0]].innerText) {
+                    cards[cardPickTwo[0]].innerText = "";
+                    playInputReset();
+                    totalscores++;
+                    score.innerText = totalscores;
+                    if (correctCardArr.length === 2) {
+                        cardPickTwo = [];
+                        symPickTwo = [];
+                        correctCardArr = [];
+                        wrongCardArr = [];
+                        makeCardSet();
+                    };
+                } else if (symTyped === cards[cardPickTwo[1]].innerText) {
+                    cards[cardPickTwo[1]].innerText = "";
+                    playInputReset();
+                    totalscores++;
+                    score.innerText = totalscores;
+                    if (correctCardArr.length === 2) {
+                        cardPickTwo = [];
+                        symPickTwo = [];
+                        correctCardArr = [];
+                        wrongCardArr = [];
+                        makeCardSet();
+                    };
                 };
+            } else if (correctCardArr.indexOf(symTyped) !== -1) {
+                console.log("you have already guess this correctly");
             };
         };
     };
